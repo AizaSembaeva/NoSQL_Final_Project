06f318a31e23b0b08d5f02e620330881634a81ef
AizaSembaeva <sembaeva2007@gmail.com>
2026-02-04 02:11:36 +0500

    feat: implement product and payment CRUD endpoints

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createPayment } from "../controllers/payment.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", createPayment);
+router.post("/", authMiddleware, createPayment);
 
 export default router;
